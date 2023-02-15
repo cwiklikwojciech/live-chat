78001d6bd40efa6481fc0404bbfc8a9499a70af6
cwiklik.wojciech <cwiklik.wojciech@gmail.com>
2023-02-15 22:19:37 +0100

    Initial commit, login, sign up form.

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import 'firebase/firestore'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyANC9LUcGunQb8zSNGMXW-14CRY691mnb0",
+    authDomain: "live-chat-b865b.firebaseapp.com",
+    projectId: "live-chat-b865b",
+    storageBucket: "live-chat-b865b.appspot.com",
+    messagingSenderId: "1069727679831",
+    appId: "1:1069727679831:web:8cc4116987b3b01fd2aa12",
+    measurementId: "G-69HXGHPQHH"
+  };
+
+  //init firebase
+
+  firebase.initializeApp(firebaseConfig)
+
+  const projectFirestore = firebase.firestore();
+
+  const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+
+  export { projectFirestore, timestamp }
